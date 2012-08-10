8a63d64a3b45c184d798e79bbf2de77ce2874348
Jose Prado <cowlby@me.com>
2012-08-09 20:01:43 -0500

    Created initial PHPUnit tests.

---
+++ tests/Cowlby/Tests/Loggly/Http/AsyncClientTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Cowlby\Tests\Loggly\Http;
+
+use Cowlby\Loggly\Http\AsyncClient;
+
+class AsyncClientTest extends \PHPUnit_Framework_TestCase
+{
+    private $input;
+    private $client;
+
+    protected function setUp()
+    {
+        $this->input = $this->getMock('Cowlby\Loggly\Input\HttpInputInterface');
+        $this->client = new AsyncClient($this->input);
+    }
+
+    protected function tearDown()
+    {
+        $this->input = null;
+        $this->client = null;
+    }
+
+    public function testGetSetInputWorks()
+    {
+        $this->assertSame($this->input, $this->client->getInput());
+    }
+}
---
+++ tests/Cowlby/Tests/Loggly/Input/HttpInputTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Cowlby\Tests\Loggly\Input;
+
+use Cowlby\Loggly\Input\HttpInput;
+use Cowlby\Loggly\Input\InputInterface;
+
+class HttpInputTest extends \PHPUnit_Framework_TestCase
+{
+    private $key = '83e527d7-fad3-4d93-89da-0c2d8c0bcd6c';
+    private $input;
+
+    /**
+     * Prepares the environment before running a test.
+     */
+    protected function setUp()
+    {
+        $this->input = new HttpInput($this->key);
+    }
+
+    /**
+     * Cleans up the environment after running a test.
+     */
+    protected function tearDown()
+    {
+        $this->input = null;
+    }
+
+    public function testGetSetKeyWorks()
+    {
+        $this->assertSame($this->key, $this->input->getKey());
+
+        $key = 'key';
+        $this->input->setKey($key);
+        $this->assertSame($key, $this->input->getKey());
+    }
+
+    public function testGetSetFormatWorks()
+    {
+        $json = 'json';
+        $this->input->setFormat($json);
+        $this->assertSame($json, $this->input->getFormat());
+
+        $text = 'text';
+        $this->input->setFormat($text);
+        $this->assertSame($text, $this->input->getFormat());
+    }
+
+    public function testFormatDefaultsToJson()
+    {
+        $this->assertEquals(InputInterface::FORMAT_JSON, $this->input->getFormat());
+    }
+
+    public function testSetFormatThrowsException()
+    {
+        $this->setExpectedException('InvalidArgumentException');
+        $this->input->setFormat('invalid');
+    }
+}
